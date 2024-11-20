f0a1a35641ae25def1cdb9890b603f7b433c087b
gaoxingsheng <gaoxingsheng>
2024-11-20 22:01:59 +0800

    11

--- client/Assets/Features/GooglePay/PurchasingManager.cs
+++ client/Assets/Features/GooglePay/PurchasingManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Framework.Base;
 using Framework.Log;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.Purchasing;
 using UnityEngine.Purchasing.Extension;
 
@@ -14,6 +15,10 @@ namespace Features.Purchasing
         private const string TAG = "PurchasingManager";
         
         IStoreController _storeController; // The Unity Purchasing system.
+        
+        Func<PurchaseEventArgs, PurchaseProcessingResult> processPurchaseCallback;
+        Action<Product, PurchaseFailureDescription> purchaseFailedCallback;
+        Action<InitializationFailureReason, string> initializedCallback;
 
         public void InitializeClient(List<string> productIds)
         {
@@ -32,7 +37,11 @@ namespace Features.Purchasing
 
         public void DoLaunchPurchaseFlow(string productId)
         {
-            _storeController.InitiatePurchase(productId);
+            var product = _storeController.products.WithID(productId);
+            var define = product.definition;
+            var productMetadata = product.metadata;
+            // var product = new Product(productDefine, productMetadata);
+            _storeController.InitiatePurchase(product, "developerPayload");
         }
 
         public void DoDumpProducts()
@@ -56,22 +65,29 @@ namespace Features.Purchasing
         public void OnInitializeFailed(InitializationFailureReason error)
         {
             LoggerEx.Debug(TAG, $"OnInitializeFailed: {error}");
+            initializedCallback?.Invoke(error, "null");
         }
 
         public void OnInitializeFailed(InitializationFailureReason error, string message)
         {
             LoggerEx.Debug(TAG, $"OnInitializeFailed: {error} : {message}");
+            initializedCallback?.Invoke(error, message);
         }
 
         public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs purchaseEvent)
         {
-            LoggerEx.Debug(TAG, $"ProcessPurchase: {purchaseEvent.purchasedProduct}");
-            return PurchaseProcessingResult.Complete;
+            LoggerEx.Debug(TAG, $"ProcessPurchase: Complete - Product{purchaseEvent.purchasedProduct}");
+            
+            if(processPurchaseCallback == null) 
+                return PurchaseProcessingResult.Complete;
+                
+            return processPurchaseCallback.Invoke(purchaseEvent);
         }
 
         public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
         {
             LoggerEx.Debug(TAG, $"OnPurchaseFailed: {product} : {failureReason}");
+            purchaseFailedCallback?.Invoke(product, new PurchaseFailureDescription("unknown", failureReason, "unknown"));
         }
 
         public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
@@ -83,6 +99,7 @@ namespace Features.Purchasing
         public void OnPurchaseFailed(Product product, PurchaseFailureDescription failureDescription)
         {
             LoggerEx.Debug(TAG, $"OnPurchaseFailed: {product} : {failureDescription}");
+            purchaseFailedCallback?.Invoke(product, failureDescription);
         }
 
         #endregion
