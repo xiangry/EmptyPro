a8cc3bc5c33575fb6e8334952c10451692bc0231
gaoxingsheng <gaoxingsheng>
2024-11-27 20:57:38 +0800

    添加benchmarktool

---
+++ tools/MyBenchMark/MyBenchMark/MyTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using Microsoft.CodeAnalysis;
+using Platform = BenchmarkDotNet.Environments.Platform;
+
+namespace MyBenchMark;
+
+
+public class NoOptimizationConfig : ManualConfig
+{
+    public NoOptimizationConfig()
+    {
+        AddJob(Job.Default
+            .WithJit(Jit.RyuJit)          // 使用旧版 JIT
+            .WithPlatform(Platform.X86));     // 强制 32 位架构
+            // .WithOptimizationLevel(OptimizationLevel.None)); // 禁用优化
+    }
+}
+
+
+
+// [MemoryDiagnoser]
+// [Config(typeof(NoOptimizationConfig))]
+public class MyTest
+{
+    private int[] data = null;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        data = new int[10000];
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = i;
+        }
+    }
+    
+    [Benchmark]
+    [MethodImpl(MethodImplOptions.NoOptimization)]
+    public int SumWithForLoop()
+    {
+        var sum = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            sum += data[i];
+        }
+
+        return sum;
+    }
+    
+    [Benchmark]
+    [MethodImpl(MethodImplOptions.NoOptimization)]
+    public int SumWithLinq()
+    {
+        return data.Sum();
+    }
+}
---
+++ tools/MyBenchMark/MyBenchMark/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using MyBenchMark;
+
+Console.WriteLine("Hello, World!");
+
+
+BenchmarkRunner.Run<MyTest>();
